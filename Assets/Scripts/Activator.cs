3f7a0b9ae62b0b960f32df77a4b6fd594bfd4863
Dmitriy Canishev <canishev.dima2019@gmail.com>
2020-09-15 23:33:19 +0300

    Puzzle Game's files
    Version 1.0 on PC. Level 1-5 and 2-1 ready now. Other will be prepared in the next version

---
+++ Assets/Scripts/Activator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour
+{
+    public GameObject[] normalGroup;
+    public GameObject[] transparentGroup;
+    public Activator button;
+    public Material normal;
+    public Material transparent;
+    public bool canPush;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (canPush)
+        {
+            if (collider.CompareTag("Barrier") || collider.CompareTag("Player")) // + collider.CompareTag("Barrier")
+            {
+                foreach (GameObject first in normalGroup)
+                {
+                    first.GetComponent<Renderer>().material = normal;
+                    first.GetComponent<Collider>().isTrigger = false;
+                }
+
+                foreach (GameObject second in transparentGroup)
+                {
+                    second.GetComponent<Renderer>().material = transparent;
+                    second.GetComponent<Collider>().isTrigger = true;
+                }
+
+                GetComponent<Renderer>().material = transparent;
+                button.GetComponent<Renderer>().material = normal;
+                button.canPush = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] KeyCode keyOne;
+    [SerializeField] KeyCode keyTwo;
+    [SerializeField] Vector3 moveDirection;
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(keyOne))
+        {
+            GetComponent<Rigidbody>().velocity += moveDirection;
+        }
+
+        if (Input.GetKey(keyTwo))
+        {
+            GetComponent<Rigidbody>().velocity -= moveDirection;
+        }
+
+        if (Input.GetKey(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        if (Input.GetKey(KeyCode.N))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(this.CompareTag("Player") && collider.CompareTag("Finish"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        if (this.CompareTag("Barrier") && collider.CompareTag("Barrier"))
+        {
+            foreach(Activator button in FindObjectsOfType<Activator>())
+            {
+                button.canPush = false;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if (this.CompareTag("Barrier") && collider.CompareTag("Barrier"))
+        {
+            foreach (Activator button in FindObjectsOfType<Activator>())
+            {
+                button.canPush = true;
+            }
+        }
+    }
+
+}
